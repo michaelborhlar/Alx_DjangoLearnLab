fdeaf01d9420a0e46995a76d19e0ecf38b1aa99d
mhykeborhlar <michealadefehinti09@gmail.com>
2025-07-31 00:24:34 +0100

    added new files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth import logout
 
 
 
-class SignUpView(CreateView):
+class register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = "register.html"
