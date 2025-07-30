42b56e26b55d8bbe9310a03554b789756214bc75
mhykeborhlar <michealadefehinti09@gmail.com>
2025-07-31 00:06:43 +0100

    added some new files

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from .views import SignUpView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
-    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail')
+    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('signup/', SignUpView.as_view(template_name = "register.html"), name='signup'),
+    path('login/', LoginView.as_view(template_name = "login.html"), name='login'),
+    path('logout/', LogoutView.as_view(template_name = "logout.html"), name='logout')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = "register.html"
+
 
 # Function-based view: List all books with authors
 def list_books(request):
