5cc53fefdfabc00645e35beb44dddc32fc8c2b5f
mhykeborhlar <michealadefehinti09@gmail.com>
2025-07-31 00:38:55 +0100

    changed some files in view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,9 +11,9 @@ from django.contrib.auth import logout
 
 
 class register(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = "register.html"
+    form_class=UserCreationForm()
+    success_url=reverse_lazy('login')
+    template_name="relationship_app/register.html"
 
 
 # Function-based view: List all books with authors
