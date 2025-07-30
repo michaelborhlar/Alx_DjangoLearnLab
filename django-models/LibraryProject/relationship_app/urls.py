d047acf8c9156c96dd2d4ffc2228a039061d21e7
mhykeborhlar <michealadefehinti09@gmail.com>
2025-07-31 00:25:57 +0100

    added a new file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', views.SignUpView.as_view(), name='register'),
+    path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name = "login.html"), name='login'),
     path('logout/', LogoutView.as_view(template_name = "logout.html"), name='logout')
 ]
