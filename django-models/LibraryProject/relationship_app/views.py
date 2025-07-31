799ea2916c58094293adef321b8f2393c5eab263
mhykeborhlar <michealadefehinti09@gmail.com>
2025-07-31 15:44:17 +0100

    added some files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,6 +11,15 @@ from django.contrib.auth.decorators import user_passes_test, login_required
 from django.shortcuts import render
 from .models import UserProfile
 
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+@login_required
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+
 
 def check_role(user, role):
     return hasattr(user, 'userprofile') and user.userprofile.role == role
