d7e49b6e1ff75a2ac5b10f6a2b75b650b5e0fda8
mhykeborhlar <michealadefehinti09@gmail.com>
2025-07-31 15:47:35 +0100

    changed some files

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name="login.html"), name='login'),
     path('logout/', LogoutView.as_view(template_name="logout.html"), name='logout'),
-    path('admin-only/', views.admin_view, name='admin_view'),
+    path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-only/', views.librarian_view, name='librarian_view'),
     path('member-only/', views.member_view, name='member_view'),
 ]
