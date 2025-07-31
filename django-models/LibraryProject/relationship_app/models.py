80eca79a884f7d19bba81c3f690aec02272ec9f8
mhykeborhlar <michealadefehinti09@gmail.com>
2025-07-31 16:22:56 +0100

    added new files

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
 
@@ -18,17 +21,26 @@ from django.contrib.auth.models import User
 #Automatic Creation: Use Django signals to automatically create a UserProfile when a new user is registered.
 
 class UserProfile(models.Model):
-    ROLE_CHOICES = (
-    ("Admin", "Admin"),
-    ("Librarian", "Librarian"),
-    ("Member", "Member"),
-    )
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=50, choices=ROLE_CHOICES)
-
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
+
+# Automatically create a profile when a new user is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
 
 
 class Author(models.Model):
