e86a2819591c0dacc97f3cd8efd9ddb95b2ccce6
mhykeborhlar <michealadefehinti09@gmail.com>
2025-07-31 18:39:55 +0100

    added some files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -16,17 +16,17 @@ def is_member(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
 # Role-based views
-@login_required
+
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'admin_view.html')
 
-@login_required
+
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'librarian_view.html')
 
-@login_required
+
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'member_view.html')
