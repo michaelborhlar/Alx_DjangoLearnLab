65c8db4a2055a689f069ffe1d7529315afb53b40
mhykeborhlar <michealadefehinti09@gmail.com>
2025-08-02 17:25:10 +0100

    added a new folder

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,18 +3,36 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
+# Create your models here.
+
+#In your Django project, you will extend the Django User model to include user roles 
+# and develop views that restrict access based on these roles. Your task is
+#  to set up this system by creating a new model for user profiles, defining views 
+# with access restrictions, and configuring URL patterns.
+
+#Step 1: Extend the User Model with a UserProfile
+#Create a UserProfile model that includes a role field with predefined roles. 
+# This model should be linked to Django’s built-in User model with a one-to-one relationship.
+
+#Fields Required:
+#user: OneToOneField linked to Django’s User.
+#role: CharField with choices for ‘Admin’, ‘Librarian’, and ‘Member’.
+#Automatic Creation: Use Django signals to automatically create a UserProfile when a new user is registered.
+
 class UserProfile(models.Model):
     ROLE_CHOICES = [
-        ('ADMIN', 'Admin'),
-        ('LIBRARIAN', 'Librarian'),
-        ('MEMBER', 'Member'),
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="MEMBER")
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
     def __str__(self):
-        return f"{self.user.username} - {self.get_role_display()}"
+        return f"{self.user.username} - {self.role}"
 
+# Automatically create a profile when a new user is created
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
@@ -22,5 +40,28 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    if hasattr(instance, 'userprofile'):
-        instance.userprofile.save()
+    instance.userprofile.save()
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+
+    author = models.ForeignKey(Author, on_delete= models.CASCADE, related_name="books", 
+                               null=True, blank = True)
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name= "library")
+
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete= models.CASCADE, null = True, blank=True)
+
+    
+
+    def __str__(self):
+        return self.name
