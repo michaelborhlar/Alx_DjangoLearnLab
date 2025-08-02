edb87fa09893d82a13c9c02edc1b7c64bd1d8dc7
mhykeborhlar <michealadefehinti09@gmail.com>
2025-08-02 20:37:47 +0100

    changed some contents in views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,7 +5,7 @@ from rest_framework import generics
 
 # Create your views here.
 
-class BookList(generics.ListCreateAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
