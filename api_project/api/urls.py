8dee5e9fb4d0c418bbfc5705db32eff1aad0c5ce
mhykeborhlar <michealadefehinti09@gmail.com>
2025-08-07 16:23:00 +0100

    added some files

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,17 +2,19 @@ from django.urls import path, include
 from .views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
+from .views import UserViewSet
 from rest_framework.authtoken.views import obtain_auth_token
 
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
+router.register(r'user_all', UserViewSet, basename='user_all')
 
 
-urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+urlpatterns = [  # Maps to the BookList view
      # Include the router URLs for BookViewSet (all CRUD operations)
     path('', include(router.urls)),  # This includes all routes registered with the router
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('user/', UserViewSet.as_view(), name='user-list'),
  
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Book
+from .models import User
 from .serializers import BookSerializer
+from .serializers import UserSerializer
 from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
@@ -10,6 +12,17 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        queryset = User.objects.all()
+        name_filter = self.request.query_params.get('name', None)
+        if name_filter is not None:
+            queryset = queryset.filter(name__icontains=name_filter)
+        return queryset
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
