4286a3754d768adb96e0d0f292d5a010fb527e7b
mhykeborhlar <michealadefehinti09@gmail.com>
2025-08-19 15:05:55 +0100

    added some new files

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -5,4 +5,3 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 # Create your views here.
+
