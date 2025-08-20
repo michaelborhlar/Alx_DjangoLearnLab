065e1390bd6623209aca0b7f03da13caef4bb215
mhykeborhlar <michealadefehinti09@gmail.com>
2025-08-20 05:59:59 +0100

    added some new files

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
-# Create your views here.
+from .forms import UserCreationForm
+
+# Registration View
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been created! You cacdn now log in.")
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "blog/register.html", {"form": form})
+
+# Profile View
+@login_required
+def profile(request):
+    return render(request, "blog/profile.html")
 
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_REDIRECT_URL = "profile"
+LOGOUT_REDIRECT_URL = "login"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
