dadc0828796851ac2890d2f21f419f26d6598eb2
mhykeborhlar <michealadefehinti09@gmail.com>
2025-09-03 13:05:05 +0100

    added new files

--- Introduction_to_Django/LibraryProject/bookshelf/urls.py
+++ Introduction_to_Django/LibraryProject/bookshelf/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .admin_view import admin
 from .member_view import member
 from .librarian_view import librarian
+from . import views
+from .views import admin_view
+
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('Admin/', admin, name="admin"),
+    path('Admin/', views.admin_view, name="admin_view"),
     path('Librarian/', librarian, name="librarian"),
     path('Member/', member, name="member"),
 ]
--- Introduction_to_Django/LibraryProject/bookshelf/views.py
+++ Introduction_to_Django/LibraryProject/bookshelf/views.py
@@ -6,5 +6,5 @@ def is_admin(user):
     return user.is_authenticated and hasattr(user, "profile") and user.profile.role == "Admin"
 @user_passes_test(is_admin, login_url="/login/")
 @login_required
-def admin(request):
+def admin_view(request):
     return render(request, "admin_view.html")
