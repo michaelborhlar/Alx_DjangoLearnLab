2ab9657ba6e8b8290bb3f3b58e4a3832196d2c3e
mhykeborhlar <michealadefehinti09@gmail.com>
2025-09-03 15:26:52 +0100

    added some new files

---
+++ social_media_api/accounts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    bio = models.TextField()
+    profile_picture = models.ImageField()
+    followers = models.ManyToManyField("self", symmetrical=False)
