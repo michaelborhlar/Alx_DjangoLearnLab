a83a5869c00af4886a0b3aeaea48b3fb48be587c
mhykeborhlar <michealadefehinti09@gmail.com>
2025-09-03 12:55:11 +0100

    added a new file

--- Introduction_to_Django/LibraryProject/bookshelf/views.py
+++ Introduction_to_Django/LibraryProject/bookshelf/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test, login_required
 # Create your views here.
+
+def is_admin(user):
+    return user.is_authenticated and hasattr(user, "profile") and user.profile.role == "Admin"
 @user_passes_test(is_admin, login_url="/login/")
 @login_required
 def admin(request):
