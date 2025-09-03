54a0470ddab8609a8d8913b27c681c2390e42697
mhykeborhlar <michealadefehinti09@gmail.com>
2025-09-03 12:52:11 +0100

    added new files

--- Introduction_to_Django/LibraryProject/bookshelf/views.py
+++ Introduction_to_Django/LibraryProject/bookshelf/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import user_passes_test, login_required
 # Create your views here.
+@user_passes_test(is_admin, login_url="/login/")
+@login_required
+def admin(request):
+    return render(request, "admin_view.html")
