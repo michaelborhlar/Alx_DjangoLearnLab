4f93e3ea21ab110fd8560659874424d9f58e93ec
mhykeborhlar <michealadefehinti09@gmail.com>
2025-09-03 17:38:48 +0100

    added some files

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,13 +2,22 @@ from .models import CustomUser
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
-User = get_user_model
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
+
+    password = serializers.Charfield(write_only=True, required=True)
     class Meta:
         model = User
         fields = "__all__"
+
+        def create(self, validated_data):
+            user = get_user_model.object.create_user()
+
+            Token.objects.create(user=user)
+            return user
  
 
 class UserSerializer(serializers.ModelSerializer):
