cce9986db7ae6f9d48998140bf1f87f746c481f4
mhykeborhlar <michealadefehinti09@gmail.com>
2025-09-03 17:45:47 +0100

    added new files

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -8,7 +8,7 @@ User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
 
-    password = serializers.Charfield()
+    password = serializers.CharField(write_only=True, required=True)
     class Meta:
         model = User
         fields = "__all__"
